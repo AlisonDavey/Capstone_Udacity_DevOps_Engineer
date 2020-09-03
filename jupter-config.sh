773c833a4bd1b52f3cbd9b7e87632b41ba946f7d
Alison Davey <alisonjanedavey@gmail.com>
2020-09-03 18:38:52 +0200

    add utf8 to ipynb

--- jupter-config.sh
+++
@@ -1,4 +0,0 @@
-jupyter notebook --generate-config
-echo 'conf = get_config()' >> ~/.jupyter/jupyter_notebook_config.py
-echo "conf.NotebookApp.ip = '0.0.0.0'" >> ~/.jupyter/jupyter_notebook_config.py
-echo 'conf.NotebookApp.port = 8888' >> ~/.jupyter/jupyter_notebook_config.py
